584a33646bdc37224721e7c2fd9e5b29103b26e2
gino0950150 <gino0950150@gmail.com>
2021-03-17 02:12:04 -0700

    mbed04 done

--- 4_2_LED_PWM/main.cpp
+++ 4_2_LED_PWM/main.cpp
@@ -2,13 +2,13 @@
 
 PwmOut PWM1(D6);
 AnalogIn Ain(A0);
-float ADCdata;
-int main(){
-  while(1){
-    PWM1.period_ms(500);
-    PWM1 = Ain;
-    ADCdata = Ain; //send an opening text message
-    printf("%f\r\n", ADCdata);
-    ThisThread::sleep_for(100ms);
-  }
+int main()
+{
+   while (1)
+   {
+      PWM1.period_ms(5);
+      PWM1 = Ain;
+      printf("%f\n\r", PWM1.read());
+      ThisThread::sleep_for(50ms);
+   }
 }
